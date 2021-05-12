877416a65339cb7c48d021c13943b4014706566b
betanaught <brendan.wakefield@gmail.com>
2021-05-12 11:56:12 -0700

    vizualizing relationships

--- rad.jl
+++ rad.jl
@@ -6,7 +6,7 @@ using CSV, DataFrames
 sensor1_data = CSV.read("data/rad_sensor1.csv", DataFrame)
 sensor2_data = CSV.read("data/rad_sensor2.csv", DataFrame)
 
-names(sensor1_data)[1:20]
+show(names(sensor1_data)[1:20])
 sensor1_data[1:10, 1:19]
 sensor1_data[1:10, ["datetime_15", "datetime_5", "Turbidity, FNU"]]
 
@@ -27,18 +27,36 @@ end
 
 show_missing(sensor1_data)
 show_missing(sensor2_data)
+
+"""
+    Exploratory Data Visualization ---------------------------------------------
+"""
+using Plots
+plotly()
+
+scatter3d
+
+scatter(sensor1_data[:, "fChl, water, in situ, RFU"], sensor1_data[:, "315.90247"], legend = false)
+
+scatter(sensor1_data[:, "fChl, water, in situ, RFU"], sensor1_data[:, "913.04602"], legend = false)
+
+cols = vcat("datetime_15", names(sensor1_data)[20:25])
+scatter3d(sensor1_data[4:100, "datetime_15"], sensor1_data[4:100, 20], sensor1_data[4:100, 21], legend = false)
+x3d = 
+scatter3d(sensor1_data[4:10, cols[1]])
 """
     Principal Component Analysis -----------------------------------------------
 """
 using Statistics, StatsBase, MultivariateStats
-using Plots
-plotly()
 
 pca_s1_data = transpose(Matrix(dropmissing(sensor1_data[:, 20:end])))
 fit_s1 = fit(PCA, pca_s1_data, maxoutdim = 10)
 fit_s1.proj
 
 scatter(fit_s1.proj, sensor1_data[:, "azimuth"], legend = false)
+
+
+
 
 # Very few PCs -> Sanity Check: confirm a few wavelengths are highly correlated
 
